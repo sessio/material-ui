01c44ef5d4b83ad6168649fa1df216cdbdecf55a
Alex Hayes <alex@alution.com>
2017-09-13 22:35:04 +1000

    [stepper] Use svg-icons/CheckCircle rather than relying on material-ui-icons. (#4799)

--- src/Stepper/StepIcon.js
+++ src/Stepper/StepIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import CheckCircle from "material-ui-icons/CheckCircle";
+import CheckCircle from "../svg-icons/CheckCircle";
 import withStyles from "../styles/withStyles";
 import StepPositionIcon from './StepPositionIcon';
 
