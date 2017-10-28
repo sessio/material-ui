bd8a2240d0e95d91758c20c3b5b6b3cd84f2b88d
Toni Miettinen <sessio@gmail.com>
2017-10-28 21:54:20 +0300

    add custom publish repo and @reliff scope to pkg name

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -38,7 +38,7 @@ function createPackageFile() {
 
       const minimalPackage = {
         ...packageDataOther,
-        name: 'material-ui',
+        name: '@reliff/material-ui',
         main: './index.js',
         module: './index.es.js',
         private: false,
